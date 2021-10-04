07faa94644df033bb22e5caf62a21476df5159da
Parin Lai <parin.lai@rblabs.io>
2021-10-04 13:32:12 +0800

    test: change Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 mkdir -p ~/.ssh && echo $SSH_KEY | base64 -d > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa && ssh-keyscan $MANIFEST_HOST >> ~/.ssh/known_hosts
 rm -rf $MANIFEST_REPO && git clone ssh://git@$MANIFEST_HOST/$MANIFEST_USER/$MANIFEST_REPO.git
 
-cd $MANIFEST_REPO/$PATH
+cd $MANIFEST_REPO/$KUSPATH
 
 for IMAGE in $(echo $IMAGES | sed "s/,/ /g")
 do
