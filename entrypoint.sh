94bdf7284da28972475d29297f3ec838bfb0a671
Parin Lai <parin.lai@rblabs.io>
2021-10-04 12:56:55 +0800

    test: change kustomize path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 mkdir -p ~/.ssh && echo $SSH_KEY | base64 -d > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa && ssh-keyscan $MANIFEST_HOST >> ~/.ssh/known_hosts
 rm -rf $MANIFEST_REPO && git clone ssh://git@$MANIFEST_HOST/$MANIFEST_USER/$MANIFEST_REPO.git
 
-git checkout $MANIFEST_BRANCH
-cd $MANIFEST_REPO/$MANIFEST_BRANCH
+cd $MANIFEST_REPO/$PATH
 
 for IMAGE in $(echo $IMAGES | sed "s/,/ /g")
 do
